4032588082ff56a76448042867de1766e3946cc8
unknown <p.mobedi@gmail.com>
2024-09-20 07:45:39 +0330

    Slide menu in Android

--- app/src/main/java/com/example/tehranbus/MainActivity.java
+++ app/src/main/java/com/example/tehranbus/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.tehranbus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.tehranbus.Database.DatabaseAssets;
 import com.example.tehranbus.Fragment.StationsFragment;
+import com.google.android.material.navigation.NavigationView;
 
 import org.greenrobot.eventbus.EventBus;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DatabaseAssets mydatabase;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_menu);
         mydatabase = new DatabaseAssets(getApplicationContext());
 
         try {
@@ -37,8 +45,31 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation);
 
+        // اضافه کردن این خط برای اتصال لیسنر
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-}
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
 
+        if (id == R.id.action_home) {
+            // کد مربوط به خانه
+            Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_LONG).show();
+        } else if (id == R.id.action_setting) {
+            // کد مربوط به تنظیمات
+            Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_LONG).show();
+        }
+        // می‌توانید شرایط بیشتری اضافه کنید
+        else {
+            // برای مواردی که با هیچکدام از شرایط بالا مطابقت ندارند
+            Toast.makeText(getApplicationContext(), "Unknown option", Toast.LENGTH_LONG).show();
+        }
+
+        // مقدار true را برگردانید تا انتخاب آیتم تایید شود
+        return true;
+    }
+    }
